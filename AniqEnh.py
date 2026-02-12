bae982763e5056d9aea4541ab18add445be4020d
danielpro432 <71192509+danielpro432@users.noreply.github.com>
2026-02-12 22:03:36 +0300

    Update AniqEnh.py

--- AniqEnh.py
+++ AniqEnh.py
@@ -121,7 +121,7 @@ class AnimatedQuotesMod(loader.Module):
             return
 
         # Автоудаление "Processing..." через 5 секунд
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(0)
         try:
             await processing_msg.delete()
         except:
