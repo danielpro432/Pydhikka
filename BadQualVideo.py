5175d5bc2272f87e6dfaa37a5edd6c57d78d0661
danielpro432 <71192509+danielpro432@users.noreply.github.com>
2026-02-15 13:16:09 +0300

    Create BadQualVideo.py

---
+++ BadQualVideo.py
@@ -0,0 +1,53 @@
+#   Coded by Dany    #
+#     t.me/Dany23s      #
+# This code under AGPL-3.0 #
+
+import os
+import random
+import string
+from .. import loader, utils
+
+@loader.tds
+class VSHAKALMod(loader.Module):
+    strings = {"name": "Video Shakal"}
+
+    @loader.owner
+    async def vshcmd(self, m):
+        ".vsh <реплай на видео> <уровень от 1 до 6 (по умолчанию 3)>\nСжакалить видео"
+
+        reply = await m.get_reply_message()
+        if not reply:
+            return await m.edit("reply...")
+        if reply.file.mime_type.split("/")[0] != "video":
+            return await m.edit("shit...")
+
+        # Сразу редактируем сообщение на "Обрабатываю..."
+        await m.edit("Обрабатываю…")
+
+        args = utils.get_args_raw(m)
+        lvls = {
+            "1": "0.1M",
+            "2": "0.08M",
+            "3": "0.05M",
+            "4": "0.03M",
+            "5": "0.02M",
+            "6": "0.01M",
+        }
+        lvl = lvls.get(args, lvls["3"])
+
+        vid = await reply.download_media(
+            "".join(random.choice(string.ascii_letters) for _ in range(25)) + ".mp4"
+        )
+
+        out = "".join(random.choice(string.ascii_letters) for _ in range(25)) + ".mp4"
+
+        os.system(
+            f'ffmpeg -y -i "{vid}" -b:v {lvl} -maxrate:v {lvl} -b:a {lvl} -maxrate:a {lvl} "{out}"'
+        )
+
+        await reply.reply(file=out)
+        await m.delete()
+
+        # Чистка
+        os.remove(vid)
+        os.remove(out) 
