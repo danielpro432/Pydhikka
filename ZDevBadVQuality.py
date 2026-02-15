5b46450724db3fbe548472d9aec0ee6da5d69e73
danielpro432 <71192509+danielpro432@users.noreply.github.com>
2026-02-15 21:37:47 +0300

    Update and rename ZDevBadVdaQuality.py to ZDevBadVQuality.py

--- ZDevBadVQuality.py
+++ ZDevBadVQuality.py
@@ -23,7 +23,6 @@ class VidQualVideo(loader.Module):
         if not reply or not reply.file:
             return await m.respond("Ошибка: нужен реплай на видео.")
 
-        # удаляем команду
         await m.delete()
 
         if reply.file.mime_type.split("/")[0] != "video":
@@ -31,28 +30,23 @@ class VidQualVideo(loader.Module):
 
         args = utils.get_args_raw(m).split()
 
-        # значения по умолчанию
         vb_input = args[0] if len(args) > 0 else "500"
         ab_input = args[1] if len(args) > 1 else "128"
 
-        # автоматическое добавление k если пользователь не написал M/k
         vb = vb_input if any(x in vb_input.lower() for x in ["k", "m"]) else f"{vb_input}k"
         ab = ab_input if any(x in ab_input.lower() for x in ["k", "m"]) else f"{ab_input}k"
 
-        # создаём случайные имена для файлов
         vid = await reply.download_media(
             "".join(random.choice(string.ascii_letters) for _ in range(25)) + ".mp4"
         )
         out = "".join(random.choice(string.ascii_letters) for _ in range(25)) + ".mp4"
 
-        # запускаем ffmpeg
+        # ffmpeg: убираем баннер и выводим только ошибки
         os.system(
-            f'ffmpeg -y -i "{vid}" -b:v {vb} -maxrate:v {vb} -b:a {ab} -maxrate:a {ab} "{out}"'
+            f'ffmpeg -y -hide_banner -loglevel error -i "{vid}" -b:v {vb} -maxrate:v {vb} -b:a {ab} -maxrate:a {ab} "{out}"'
         )
 
-        # отправка видео
         await reply.reply(file=os.path.abspath(out))
 
-        # удаляем временные файлы
         os.remove(vid)
         os.remove(out)
