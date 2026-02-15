278b5dcc02cce944444095287228352cdd392e70
danielpro432 <71192509+danielpro432@users.noreply.github.com>
2026-02-15 13:27:06 +0300

    Update and rename BadQualVideo.py to BadQualVideos.py

--- BadQualVideos.py
+++ BadQualVideos.py
@@ -1,6 +1,7 @@
-#   Coded by Dany    #
-#     t.me/Dany23s      #
-# This code under AGPL-3.0 #
+#  Dany23s   
+# хуй
+#член
+# нет0
 
 import os
 import random
@@ -8,21 +9,18 @@ import string
 from .. import loader, utils
 
 @loader.tds
-class VSHAKALMod(loader.Module):
-    strings = {"name": "Video Shakal"}
+class VideoQual(loader.Module):
+    strings = {"name": "VidLow-quality"}
 
     @loader.owner
-    async def vshcmd(self, m):
-        ".vsh <реплай на видео> <уровень от 1 до 6 (по умолчанию 3)>\nСжакалить видео"
+    async def qvlcmd(self, m):
+        ".qvl <реплай на видео> <уровень от 1 до 6 (по умолчанию 3)>\n "
 
         reply = await m.get_reply_message()
         if not reply:
-            return await m.edit("reply...")
+            return
         if reply.file.mime_type.split("/")[0] != "video":
-            return await m.edit("shit...")
-
-        # Сразу редактируем сообщение на "Обрабатываю..."
-        await m.edit("Обрабатываю…")
+            return
 
         args = utils.get_args_raw(m)
         lvls = {
@@ -38,7 +36,6 @@ class VSHAKALMod(loader.Module):
         vid = await reply.download_media(
             "".join(random.choice(string.ascii_letters) for _ in range(25)) + ".mp4"
         )
-
         out = "".join(random.choice(string.ascii_letters) for _ in range(25)) + ".mp4"
 
         os.system(
@@ -46,8 +43,8 @@ class VSHAKALMod(loader.Module):
         )
 
         await reply.reply(file=out)
-        await m.delete()
+        await m.delete()  # команда удаляется, никакого текста
 
-        # Чистка
+        # Чистка файлов
         os.remove(vid)
-        os.remove(out) 
+        os.remove(out)
